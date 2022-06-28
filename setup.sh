1a790108a43b0d4f5f402c49ae6725d3f47560be
Snehan Kekre <snehan.minerva@gmail.com>
2022-06-28 16:13:51 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,13 +3,17 @@
 mkdir -p pretrained
 
 # download vqgan
-curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
-apt-get install git-lfs
-git lfs install
-git clone https://huggingface.co/dalle-mini/vqgan_imagenet_f16_16384 ./pretrained/vqgan
+curl https://huggingface.co/dalle-mini/vqgan_imagenet_f16_16384/resolve/main/flax_model.msgpack -L --output ./pretrained/vqgan/flax_model.msgpack
+curl https://huggingface.co/dalle-mini/vqgan_imagenet_f16_16384/raw/main/config.json -L --output ./pretrained/vqgan/config.json
 
 # download dalle-mini and dalle mega
-git clone https://huggingface.co/snehan/dalle_bart_mini ./pretrained/dalle_bart_mini
+curl https://huggingface.co/snehan/dalle_bart_mini/resolve/main/flax_model.msgpack -L --output ./pretrained/dalle_bart_mini/flax_model.msgpack
+curl https://huggingface.co/snehan/dalle_bart_mini/raw/main/config.json -L --output ./pretrained/dalle_bart_mini/config.json
+curl https://huggingface.co/snehan/dalle_bart_mini/raw/main/merges.txt -L --output ./pretrained/dalle_bart_mini/merges.txt
+curl https://huggingface.co/snehan/dalle_bart_mini/raw/main/special_tokens_map.json -L --output ./pretrained/dalle_bart_mini/special_tokens_map.json
+curl https://huggingface.co/snehan/dalle_bart_mini/raw/main/tokenizer.json -L --output ./pretrained/dalle_bart_mini/tokenizer.json
+curl https://huggingface.co/snehan/dalle_bart_mini/raw/main/tokenizer_config.json -L --output ./pretrained/dalle_bart_mini/tokenizer_config.json
+curl https://huggingface.co/snehan/dalle_bart_mini/raw/main/vocab.json -L --output ./pretrained/dalle_bart_mini/vocab.json
 ls -la
 ls -la pretrained/
 ls -lh pretrained/dalle_bart_mini/
