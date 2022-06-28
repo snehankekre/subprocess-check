a9d318643230c9224a1598ae158981723b843409
Snehan Kekre <snehan.minerva@gmail.com>
2022-06-28 16:01:09 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,4 +12,4 @@ git clone https://huggingface.co/dalle-mini/vqgan_imagenet_f16_16384 ./pretraine
 git clone https://huggingface.co/snehan/dalle_bart_mini ./pretrained/dalle_bart_mini
 ls -la
 ls -la pretrained/
-ls -la pretrained/dalle_bart_mini/
+ls -lh pretrained/dalle_bart_mini/
