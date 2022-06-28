3b16699cfd4a9b9fc0310813902bcd8213598f78
Snehan Kekre <snehan.minerva@gmail.com>
2022-06-28 16:19:24 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 mkdir -p pretrained
+mkdir -p pretrained/vqgan
+mkdir -p pretrained/dalle_bart_mini
 
 # download vqgan
 curl https://huggingface.co/dalle-mini/vqgan_imagenet_f16_16384/resolve/main/flax_model.msgpack -L --output ./pretrained/vqgan/flax_model.msgpack
